71a2f7b70f38d9008b8b18cc2942d06c272ddf85
ewertonBernardo <ewerton.learning@gmail.com>
2022-08-10 15:14:35 -0300

    mark primes smaller

--- sieve_eratosthenes/sieve_eratosthenes.py
+++ sieve_eratosthenes/sieve_eratosthenes.py
@@ -1,6 +1,23 @@
-num = input("Number to show primes smaller than it:")
-#print(int(num))
+num = int(input("Number to show primes smaller than it:"))
+smallers = [True]*(num)
+#print(len(smallers))
 
-smallers = [True]*(int(num))
 
-print(len(smallers))
+for i in range(num):
+	#print(f"i:{i}")
+	if(i > 0):
+		
+		if(smallers[i] == True):
+			p = (i+1)*(i+1)-1
+			if(p<num):
+				smallers[p] = False
+
+			for p in range(p, num,(i+1)):
+				#print(f"p:{p}")
+				if(p<num):
+					smallers[p] = False
+	else:
+		smallers[i] = False
+
+for i in range(num):
+	print(smallers[i])
