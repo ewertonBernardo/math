02d02a183f2b4ddf1c89427450259583439f582d
ewertonBernardo <ewerton.learning@gmail.com>
2022-08-16 13:40:09 -0300

    adjustmemt and comments

--- sieve_eratosthenes/sieve_eratosthenes.py
+++ sieve_eratosthenes/sieve_eratosthenes.py
@@ -4,20 +4,26 @@ smallers = [True]*(num)
 
 
 for i in range(num):
-	#print(f"i:{i}")
 	if(i > 0):
-		
+		#next prime
 		if(smallers[i] == True):
+			print(f"multiples of {i+1}")
 			p = (i+1)*(i+1)-1
-			if(p<num):
+			#discard first multiple of p
+			if(p<num and smallers[p]==True):
 				smallers[p] = False
+				print(p+1)
 
+			#go through the numbers 2 by 2, 3 by 3, 5 by 5...
+			p += (i+1)
 			for p in range(p, num,(i+1)):
-				#print(f"p:{p}")
-				if(p<num):
+				if(p<num and smallers[p]==True):
+					#discard multiple of p
 					smallers[p] = False
+					print(p+1)
+	#number 1				
 	else:
 		smallers[i] = False
-
+# view primes in list(True)
 for i in range(num):
-	print(smallers[i])
+	print(f"{i+1}: "+str(smallers[i]))
