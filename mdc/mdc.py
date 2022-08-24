0a8a5aa2037b0454c6229e8caf2a03b70dc7776b
ewertonBernardo <ewerton.learning@gmail.com>
2022-08-24 16:19:30 -0300

    idea about MDC

---
+++ mdc/mdc.py
@@ -0,0 +1,25 @@
+
+numbers = []
+while True:
+	number = int(input())
+	if number == 0:
+		break
+	else:
+		numbers.append(number)
+
+
+multiples = []
+
+if(numbers[0]%2 == 0 and numbers[0]!=1):
+	numbers[0] = numbers[0]/2
+	multiples.append(2)
+if(numbers[0]%3 == 0 and numbers[0]!=1):
+	numbers[0] = numbers[0]/3
+	multiples.append(3)
+if(numbers[0]%5 == 0 and numbers[0]!=1):
+	numbers[0] = numbers[0]/5
+	multiples.append(5)
+
+
+for m in multiples:
+	print(m)
